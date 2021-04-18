1e821feb4a724ca73e6fba9bd92de5677ad8b868
quena <quena.u.s@gmail.com>
2021-04-18 18:45:44 +0200

    initial button functionality

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const images = document.getElementsByClassName('carousel-image');
-let imagePosition = 0; //starting index of carousel
-const totalSlides = images.length;
+const images = document.getElementsByClassName('carousel-image')
+let imagePosition = 0 //starting index of carousel
+const totalImages = images.length
+
+const nextButton = document.getElementById("next-button")
+const prevButton = document.getElementById("prev-button")
+
+nextButton.addEventListener("click", nextSlide)
+prevButton.addEventListener("click", previousSlide)
+
+function nextSlide() {
+    if (imagePosition === totalImages - 1) {
+        imagePosition = 0
+    } else {
+        imagePosition++
+    }
+    images[imagePosition].classList.add("visible-image")
+}
+
+function previousSlide() {
+
+}
