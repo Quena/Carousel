fad82f7766812d0a6053c88679aa7d04eafc766f
quena <quena.u.s@gmail.com>
2021-04-18 22:00:04 +0200

    added more functionality to buttons, now both next and prev buttons work

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const prevButton = document.getElementById("prev-button")
 nextButton.addEventListener("click", nextSlide)
 prevButton.addEventListener("click", previousSlide)
 
+function hideAll(){
+    for(let i = 0; i < totalImages; i++){
+        images[imagePosition].classList.remove("visible-image")
+        images[imagePosition].classList.add("hidden-image")
+    }
+}
 function nextSlide() {
+    hideAll()
     if (imagePosition === totalImages - 1) {
         imagePosition = 0
     } else {
@@ -18,5 +25,11 @@ function nextSlide() {
 }
 
 function previousSlide() {
-
+    hideAll()
+if (imagePosition === 0) {
+    imagePosition = totalImages -1
+} else {
+    imagePosition--
+}
+images[imagePosition].classList.add("visible-image")
 }
